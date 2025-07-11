16747a795a9571b96b80aa7f32fc79950d60aa51
Rodrigo <rod_novaes@hotmail.com>
2025-07-11 18:33:57 +0200

    Add search functionality

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,7 +1,7 @@
 import { defineQuery } from "next-sanity";
 
 export const STARTUP_QUERY =
-  defineQuery(`*[_type == 'startup'  && defined(slug.current)] | order(createdAt desc) {
+  defineQuery(`*[_type == 'startup'  && defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search] | order(createdAt desc) {
   _id, 
   title, 
   slug, 
